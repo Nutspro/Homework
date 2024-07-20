20db0c59bcce28a3c81bd3f585c604c55b5b2401
Nutspro <kuvatova.n.n@mail.ru>
2024-07-21 00:35:07 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+cours_name = 'Python'
+time_per_homework = hours_count / homework_count
+print('Курс:', str(cours_name) + ', всего задач:' + str(homework_count) + ', затрачено часов:', str(hours_count) + ', среднее время выполнения', time_per_homework, 'часа.')
