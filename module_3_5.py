6746233fce2cf508809a976d295e38dfbc104a83
Nutspro <kuvatova.n.n@mail.ru>
2024-08-14 15:42:01 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif first == 0:
+        return 1  # иначе в случае 0 на конце number обнулится всё произведение
+    else:
+        return first
+result = get_multiplied_digits(402030)
+print(result)
